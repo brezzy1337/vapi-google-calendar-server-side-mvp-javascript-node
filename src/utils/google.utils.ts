ef103fc457ce98652a0aa648e325734c752c7113
Devin Phat <90067281+brezzy1337@users.noreply.github.com>
2024-05-25 21:40:36 +0000

    Set up googleCredentials in google.utils.ts and got rid or the config dupicate. I am not sure if the handler using the google oAuth client will recongize the credentials be set. If there is a credentials error from google we can try to move the credentials setup to google-calendar.ts

--- src/utils/google.utils.ts
+++ src/utils/google.utils.ts
@@ -6,3 +6,9 @@ export const oAuth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_SECRET,
   process.env.GOOGLE_REDIRECT_URI
 );
+
+// Set the access token (you might need to get this from your authentication flow)
+oAuth2Client.setCredentials({
+  access_token: process.env.GOOGLE_ACCESS_TOKEN,
+  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+});
